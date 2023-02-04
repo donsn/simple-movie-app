3140b9b65967247cd8dddffd3cb5ae2134d6c3fa
Nwachukwu Daniel (DONSN) <31552644+donsn@users.noreply.github.com>
2023-02-04 20:29:39 +0100

    Styled Signup Page

--- frontend/src/pages/auth/login.js
+++ frontend/src/pages/auth/login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Button } from '../../components/buttons';
 import { Form } from '../../components/forms';
 import { InputField } from '../../components/inputs';
@@ -12,6 +13,7 @@ export default function LoginPage() {
         <InputField label="Username" name="username" type="text" />
         <InputField label="Password" name="password" type="password" />
         <Button> Login</Button>
+        <Link to="/register">Don't have an account? Signup</Link>
       </Form>
   </FlexContainer>
   );
--- frontend/src/pages/auth/signup.js
+++ frontend/src/pages/auth/signup.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '../../components/buttons';
+import { Form } from '../../components/forms';
+import { InputField } from '../../components/inputs';
+import { FlexContainer } from './styles';
 
 export default function SignupPage() {
   return (
-    <>
-    
-    </>
+    <FlexContainer>
+      <Form>
+        <h1>Signup</h1>
+        <InputField label="Username" name="username" type="text" />
+        <InputField label="Password" name="password" type="password" />
+        <Button> Signup</Button>
+        <Link to="/login">Already have an account? Login</Link>
+      </Form>
+    </FlexContainer>
   );
 }
