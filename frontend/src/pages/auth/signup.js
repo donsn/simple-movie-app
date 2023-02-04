3a61c8ea4f1a0ae9ecd8cc2adfa24073bbd889f0
Nwachukwu Daniel (DONSN) <31552644+donsn@users.noreply.github.com>
2023-02-04 21:11:05 +0100

    Implemented Signup

--- frontend/src/pages/auth/signup.js
+++ frontend/src/pages/auth/signup.js
@@ -1,19 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../../components/buttons';
 import { Form } from '../../components/forms';
 import { InputField } from '../../components/inputs';
 import { FlexContainer } from './styles';
+import { useFormik } from 'formik';
+import { useCreateUserMutation } from '../../api/users';
+import { showMessage, MessageTypes } from '../../components/toast';
+
 
 export default function SignupPage() {
+  const navigate = useNavigate();
+  const [registerUser, { isLoading }] = useCreateUserMutation();
+
+  const handleSignup = (values) => {
+    const { name, username, password } = values;
+    registerUser({ name, username, password })
+    .unwrap()
+    .then((result) => {
+      if (result.status) {
+        showMessage(MessageTypes.SUCCESS, 'Signup Successful. Please login.');
+        navigate('/login');
+      }else{
+        showMessage(MessageTypes.ERROR, result.message);
+      }
+    });
+    return;
+  };
+
+  const formHandler = useFormik({
+    initialValues: {
+      name: '',
+      username: '',
+      passwordHash: '',
+    },
+    onSubmit: handleSignup,
+  });
+
   return (
     <FlexContainer>
-      <Form>
+      <Form onSubmit={formHandler.handleSubmit}>
         <h1>Signup</h1>
-        <InputField label="Full Name" name="name" type="text" required/>
-        <InputField label="Username" name="username" type="text" required/>
-        <InputField label="Password" name="password" type="password" required/>
-        <Button> Signup</Button>
+        <InputField label="Full Name" name="name" type="text" required onChange={formHandler.handleChange} value={formHandler.values.name}/>
+        <InputField label="Username" name="username" type="text" required onChange={formHandler.handleChange} value={formHandler.values.username}/>
+        <InputField label="Password" name="passwordHash" type="password" required onChange={formHandler.handleChange} value={formHandler.values.passwordHash}/>
+        <Button type='submit' disabled={isLoading}> {isLoading ? 'Loading...' : 'Signup'}</Button>
         <Link to="/login">Already have an account? Login</Link>
       </Form>
     </FlexContainer>
