49ecf17bc4e9efc693905e456b9479b5f02ec665
Nwachukwu Daniel (DONSN) <31552644+donsn@users.noreply.github.com>
2023-02-04 12:05:24 +0100

    Fixed Movie To Use Live Data

--- frontend/src/components/movie/small.js
+++ frontend/src/components/movie/small.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { slugify } from '../../utilities/slugify';
+import { BASE_URL } from '../../api/base';
 import Rating from '../rating';
 import { SmallMovieContainer, MovieTitle, MoviePosterContainer, MovieDetailsContainer } from './styles';
 
 export default function SmallMovieView({ movie }) {
   return (
-    <SmallMovieContainer to={`/movies/${slugify('Spider Man - Far From Home')}`}>
-      <MovieTitle>Spider Man - Far From Home</MovieTitle>
+    <SmallMovieContainer to={`/movies/${movie.slug}`}>
+      <MovieTitle>{movie.name}</MovieTitle>
       <MoviePosterContainer>
-        <img src="https://via.placeholder.com/300x450" alt="Movie Poster" />
+        <img src={`${BASE_URL}/images/${movie.photo}`} alt="Movie Poster" />
       </MoviePosterContainer>
       <MovieDetailsContainer>
-        <p>Released: {new Date().getFullYear()}</p>
-        <Rating rating={3} />
+        <p>Released: {new Date(movie.releaseDate).getFullYear()}</p>
+        <Rating rating={movie.rating} />
       </MovieDetailsContainer>
     </SmallMovieContainer>
   );
